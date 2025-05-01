883f04383c4be43b86bdc7c2c67ca15413330a2b
ejsadiarin <ejs@ejsadiarin.com>
2025-05-01 15:29:14 +0800

    chore: add proxy

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     output: 'standalone',
     /* config options here */
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: `http://event-api.default.svc.cluster.local/:path*`,
+            },
+        ]
+    },
 };
 
 export default nextConfig;
